1f22dd33f788b835b4b19fc79608c6d4c5d09fee
samuel <samueloyelade6@gmail.com>
2024-03-12 19:16:31 +0100

    first commit

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ function check() {
      score.value
           if (score.value <= 39 && score.value >=0 ) {
              result.innerHTML = "fail"
-             result.style.color= "red"
+             result.style.color= "red😥😭"
           }
             else if(score.value >=40 && score.value < 45 ) {
              result.innerHTML = "pass"
-             result.style.color= "black"
+             result.style.color= "black🙄🙄"
           }
            else if (score.value >=45 && score.value < 50 ) {
-             result.innerHTML = "average"
+             result.innerHTML = "average😚😙"
              result.style.color="yellow"
           }
            else if (score.value >=50 && score.value < 60 ) {
-             result.innerHTML = "credit"
+             result.innerHTML = "credit🤗😜"
              result.style.color= "blue"
           }
            else if (score.value >=60 && score.value < 70 ) {
-             result.innerHTML = "good"
+             result.innerHTML = "good😎😬"
              result.style.color= "gold"
           }
            else if (score.value >=70 && score.value <=100 ) {
-             result.innerHTML = "excellent"
+             result.innerHTML = "excellent🥵🥵"
              result.style.color= "green"
           }
           else{
